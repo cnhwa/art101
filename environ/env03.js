30da51ce7d6d0b97d13e030a2758674fda0b3a62
cnhwa <cnhwang@ucsc.edu>
2026-05-19 18:18:46 -0700

    env03 done?

--- environ/env03.js
+++ environ/env03.js
@@ -1,11 +1,31 @@
-let environmentTitle = "a shooting star? no! its a cat!";
+let environmentTitle = "a shooting star? no! its a cat! a paper cat!";
 
-let environmentElements = ["tree", "rock", "grass", "stars", ""];
+let environmentElements = ["tree", "rock", "grass", "stars", "clouds", "moon"];
 
 let mainEntity = {
     name: "the cat",
     type: "animal",
     mood: "ambitous",
     isMoving: true,
-    favoriteElement: environmentElements[1]
+    favoriteElement: environmentElements[3]
 };
+
+let messages = {
+    title: "info: a shooting star? no! its a cat! a paper cat!",
+    "main-entity": "info: a cat that loves to practice its jumping every night, and is made of paper",
+    moon: "info: it's made of rocks and floats in space",
+    tree: "info: it's a tree",
+    rock: "info: it's a rock, it sits there all day long",
+    clouds: "info: made of water, they float through the sky for hours",
+    stars: "info: bright shiny objects the paper cat loves to watch"
+};
+
+$("#button-row button").click(function () {
+    let id = $(this).attr("id");
+    let message = messages[id];
+
+    $("#message").text(message);
+
+    $("#button-row button").removeClass("selected");
+    $(this).addClass("selected");
+});
--- garden/creature.js
+++ garden/creature.js
@@ -1,71 +1,80 @@
     // reminder "cmd /" to mass comment/uncomment parts
-    let allCreatures=[];
+   // store all creatures in an array
 
-    // function to grab data from the form
-    function getCreatureFromForm(){
-        const freshCreature={
-            name: $("#crName").val(),
-            color: $("#crColor").val(),
-            eyesNum: $("#crEyesNum").val(),
-        }
-        return freshCreature; 
+let allCreatures=[];
+
+
+// functions
+// function to grab data from the form
+function getCreatureFromForm(){
+  
+  const freshCreature={
+        name: $("#crName").val(),
+        color: $("#crColor").val(),
+        eyesNum: $("#crEyesNum").val()
+    };
+
+return freshCreature;
+};
+
+// the checks function
+function isCreatureValid(creature) {
+  if (creature.name === "" ) return false;
+  if (creature.name.length >12) return false;
+  if ( isNaN(creature.eyesNum) || creature.eyesNum >5 ) return false;
+  return true;
+}
+
+// functions add creature to the page
+function renderCreature(creature) {
+
+    let crEyesHtml="";
+    for (let i = 0; i < creature.eyesNum; i++) {
+        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
     }
 
-    //function to check everything
-    function isCreatureValid(creature) {
-        if (creature.name === "" ) return false;
-        if (creature.name.length >12) return false;
-        if ( isNaN(creature.eyesNum) || creature.eyesNum >5 ) return false;        
-        return true;
+    const html=`
+        <div class="creature">
+            <div class="creature-body" style="background-color: ${creature.color}"> ${crEyesHtml} </div>
+            <div class="creature-info">${creature.name}</div>
+        </div>
+    `;
+
+    return html;
+}
+
+// append creature to the page
+function addCreatureToDOM(creature) {
+    const html=renderCreature(creature);
+    $("#creature-list").append(html);
+}
+
+
+
+// the main brain
+$("#crAdd").click( function(){
+
+    // create creature object from the form inputs
+    const newCreature = getCreatureFromForm();
+    console.log(newCreature);
+  
+  // safety checks
+    console.log( isCreatureValid(newCreature) );
+    if ( isCreatureValid(newCreature)==false ) { // true==false 
+     return; // stops the function which is "click" one
     }
 
-    //function to add creature to the page
-    function renderCreature(creature){
-        let crEyesHtml = "";
-        for(let i = 0; i < creature.eyesNum; i++){
-            crEyesHtml = crEyesHtml + "<div class='eye'>.<div>";
+    // add creature to the page
+    addCreatureToDOM(newCreature)
 
-        }
+    // save to the memory 
+    allCreatures.push(newCreature);
+    addCreatureToDOM(newCreature);
+    clearForm();
 
-        const html=`
-            <div class="creature">
-                <div class="creature-body" style="background-color:${crColor}"> ${crEyesHtml} </div>
-                <div class="creature-info"> ${crName}</div> 
-            </div>
-            
-            `;
-        return html;
-    }
+    // reset the fporm prepare for the next iteration
 
-    //function to append creature to page
-    function addCreatureToDOM(creature){
-        const html = renderCreature(creature);
-        $("#creature-list").append(html);
-    }
-
-    // function clearForm(){
-    //     $("#crName").val("");
-    //     $("#crColor").val("#ee7dea");
-    //     $("#crEyesNum").val(1);
-    // }
-
-    $("#crAdd").click(function(){
-        // create creature object from the form inputs
-        const newCreature = getCreatureFromForm();
-        console.log(newCreature);
-        
-        // safety checks
-        console.log(isCreatureValid(newCreature));
-        if (isCreatureValid(newCreature)==false){
-            return;
-        }
-
-        // add creature to the page
-        addCreatureToDOM(newCreature);
-
-        // save to the memory
-        // reset the form to prepare for the next iteration
-    });
+});
 
 
 
